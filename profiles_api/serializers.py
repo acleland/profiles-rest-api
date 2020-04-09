b07c60f21d678b5637abc9e5c7c87df6a4f51e9f
Andrea Cleland <andreaclelan@gmail.com>
2020-04-09 16:59:23 -0700

    not much changed

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
+"""
+A serializer is a feature of django rest framework
 
+data input  --->  python object  (and vice versa)
+
+similar to a django form. - define fields for your input to your api.
+
+"""
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
